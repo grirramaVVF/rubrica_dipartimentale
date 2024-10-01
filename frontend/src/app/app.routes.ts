a9660c3fd1232658f261cfe1de5558f0dfca4ffe
Peppe M. <giuseppe.merola@vigilfuoco.it>
2024-10-01 11:19:50 +0200

    - aggiunto link al login component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { SedeComponent } from './components/sede/sede.component';
 import { TestbeComponent } from './components/testbe/testbe.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
     { path: "home", component: HomeComponent },
     { path: "sedi", component: SedeComponent },
     { path: "testbe", component: TestbeComponent },
+    { path: "login", component: LoginComponent},
     { path: '**', redirectTo: '/home', pathMatch: 'full' },
 ];
